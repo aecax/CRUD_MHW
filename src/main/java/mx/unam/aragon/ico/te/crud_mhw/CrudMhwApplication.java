f42d230d7a55109709ee7595b79da03a25cba14c
aecax <angelenriquexd50@gmail.com>
2025-05-26 11:53:00 -0600

    Commit inicial del proyecto

---
+++ src/main/java/mx/unam/aragon/ico/te/crud_mhw/CrudMhwApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.te.crud_mhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMhwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMhwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/unam/aragon/ico/te/crud_mhw/CrudMhwApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.te.crud_mhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMhwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
